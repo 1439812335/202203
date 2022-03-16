e3e7c73919156fe4375a0343efe6d50051b5d04b
14398 <1439812335@qq.com>
2022-03-16 16:45:10 +0800

    123

---
+++ src/main/java/com/bjpowernode/domain/person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+public class person {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
